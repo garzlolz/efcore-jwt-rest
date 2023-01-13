9f45768b7529c5c938d4c3f6204cb4942769ce04
garz <oscar861213@gmail.com>
2023-01-13 10:42:05 +0800

    add connectionString to json

--- Context/PubsContext.cs
+++ Context/PubsContext.cs
@@ -21,9 +21,13 @@ public partial class PubsContext : DbContext
     //public virtual DbSet<Job> Jobs { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost;Database=pubs;User Id=pub_ad;Password=1213;Trust Server Certificate=true;");
-
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+           .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+           .AddJsonFile("appsettings.json")
+           .Build();
+        optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnectionString"));
+    }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Employee>(entity =>
