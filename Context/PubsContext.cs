f86d4519ae10f1291dcf7d32e1681b723d8b3b09
garz <oscar861213@gmail.com>
2023-01-16 11:09:18 +0800

    Add Configure Service

--- Context/PubsContext.cs
+++ Context/PubsContext.cs
@@ -19,8 +19,7 @@ public partial class PubsContext : DbContext
     public virtual DbSet<Employee> Employees { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-         => optionsBuilder.UseSqlServer("Server=localhost;Database=pubs;User Id=pub_ad;Password=1213;Trust Server Certificate=true;");
+        => optionsBuilder.UseSqlServer("Name=ConnectionStrings:Pubs");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
